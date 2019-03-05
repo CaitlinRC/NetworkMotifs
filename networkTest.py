efec4d31cb77ea489791b496e870a8bc28f6df38
CaitlinRC <caitlinburns2308@gmail.com>
2019-03-05 00:16:41 +0000

    Potential working of motif 030T

--- networkTest.py
+++ networkTest.py
@@ -76,8 +76,92 @@ def triad021U(currentGraph, numberOfOccurences):
     # Finds this type, number of occurences to reduce search time
 
 def triad030T(currentGraph, numberOfOccurences):
-    print("running")
+
+    # 1 to 2, 1 to 3, 3 to 2
     # Finds this type, number of occurences to reduce search time
+
+    triadOptions = {}
+    items = []
+
+    for i in currentGraph.nodes():
+        items = []
+        counter = 0
+        val = currentGraph.neighbors(i)
+
+        for option in val:
+            items.append(int(option))
+            counter += 1
+
+
+        if counter >= 2:
+
+            triadOptions[int(i)] = items
+
+
+    # Needs to go through list of nodes and neighbors, find those that are
+    # Find those that go from 1 to 2, 1 to 3, 3 to 2
+    # For each node in triadOptions
+    # Assign one as value a, its connections as b and c. If b and c also connect
+
+
+
+    triadFound = False
+    print(triadOptions)
+    aValue = None
+    bValue = None
+    cValue = None
+
+    # avalue is value that goes to bvalue and cvalue
+    # cvalue goes to bvalue
+
+    # Currently not working ? Selecting wrong values
+    # Need to make this work grr
+
+    while triadFound == False:
+
+        triadList = []
+
+        for option in triadOptions: # Looking through each value (potentially A)
+
+            for item in triadOptions[option]:
+
+                triadList.append(item)
+
+            for choice in triadList:
+
+                if (choice in triadOptions and option in triadOptions[choice]):
+
+                    aValue = option
+
+                    cValue = choice
+
+                for myChoice in triadList:
+
+                    if (aValue != None) and (cValue != None) and (myChoice in triadOptions[aValue] and myChoice in triadOptions[cValue]):
+                        bValue = myChoice
+
+
+        if (aValue != None and bValue != None and cValue != None):
+            triadFound = True
+
+    print(aValue, bValue, cValue)
+    new = nx.DiGraph()
+
+    new.add_node(aValue)
+    new.add_node(bValue)
+    new.add_node(cValue)
+    new.add_edge(aValue, bValue)
+    new.add_edge(cValue, aValue)
+    new.add_edge(cValue, bValue)
+
+    plt.cla()
+    nx.draw(new, pos = None, arrows=True, with_labels=True)
+    plt.savefig('Triad Found')
+
+
+
+
+
 
 def triad120U(currentGraph, numberOfOccurences):
     print("running")
@@ -110,7 +194,10 @@ plt.savefig('mtxExample')
 
 e = nx.triadic_census(currentGraph)
 print(e)
-
+plt.cla()
+H = currentGraph.subgraph(['1', '58', '45'])
+nx.draw(H, pos = None, arrows=True, with_labels=True)
+plt.savefig('compare')
 
 # Null triad = Type 003
 # Dyadic Traids = Type 012 and 102
