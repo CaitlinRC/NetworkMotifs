911698661e1e565bb220c226e2417479aacc8634
CaitlinRC <caitlinburns2308@gmail.com>
2019-03-11 16:39:46 +0000

    Merged Spyros's code with original

--- networkTest.py
+++ networkTest.py
@@ -1,212 +1,69 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def findTriad(currentGraph, triad, occurences):
+def getting_Triangles(G):
 
-    if (triad == '111D'):
-        triad111D(currentGraph, occurences)
+    m = {v: i for i, v in enumerate(G)}
+    for v in G:
+        vnbrs = set(G.pred[v]) | set(G.succ[v])
+        for u in vnbrs:
+            if m[u] <= m[v]:
+                continue
+            neighbors = (vnbrs | set(G.succ[u]) | set(G.pred[u])) - {u, v}
 
-    elif (triad == "201"):
-        triad201(currentGraph, occurences)
+            # Count connected triads.
+            for w in neighbors:
+                if m[u] < m[w] or (m[v] < m[w] < m[u] and
+                                   v not in G.pred[w] and
+                                   v not in G.succ[w]):
+                    yield v, u, w
 
-    elif (triad == "210"):
-        triad210(currentGraph, occurences)
 
-    elif (triad == "300"):
-        triad300(currentGraph, occurences)
+def tricode(G, v, u, w):
 
-    elif (triad == "021D"):
-        triad021D(currentGraph, occurences)
+    combos = ((v, u, 1), (u, v, 2), (v, w, 4), (w, v, 8), (u, w, 16),
+              (w, u, 32))
+    return sum(x for u, v, x in combos if v in G[u])
 
-    elif (triad == "111U"):
-        triad111U(currentGraph, occurences)
+G = nx.read_adjlist('ca-sandi_auths.mtx', create_using=nx.DiGraph())
 
-    elif (triad == "120D"):
-        triad120D(currentGraph, occurences)
+print("Is directed: ", nx.is_directed(G))
+print(nx.number_of_nodes(G), " nodes")
+triads = nx.triadic_census(G)
+print("Triad: Occurences")
 
-    elif (triad == "021U"):
-        triad021U(currentGraph, occurences)
+for i in triads:
+    if (triads[i] != 0) and (i != '003') and (i != '012') and (i != '102'):
+        print(i, " : ", triads[i])
 
-    elif (triad == "030T"):
-        triad030T(currentGraph, occurences)
+print("-------------")
 
-    elif (triad == "120U"):
-        triad120U(currentGraph, occurences)
 
-    elif (triad == "021C"):
-        triad021C(currentGraph, occurences)
 
-    elif (triad == "030C"):
-        triad030C(currentGraph, occurences)
+TRICODES = (1, 2, 2, 3, 2, 4, 6, 8, 2, 6, 5, 7, 3, 8, 7, 11, 2, 6, 4, 8, 5, 9,
+            9, 13, 6, 10, 9, 14, 7, 14, 12, 15, 2, 5, 6, 7, 6, 9, 10, 14, 4, 9,
+            9, 12, 8, 13, 14, 15, 3, 7, 8, 11, 7, 12, 14, 15, 8, 14, 13, 15,
+            11, 15, 15, 16)
 
-    else:
-        triad120C(currentGraph, occurences)
+#: important: it corresponds to the tricodes given in :data:`TRICODES`.
+TRIAD_NAMES = ('003', '012', '102', '021D', '021U', '021C', '111D', '111U',
+               '030T', '030C', '201', '120D', '120U', '120C', '210', '300')
 
 
-def triad111D(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
+#: A dictionary mapping triad code to triad name.
+TRICODE_TO_NAME = {i: TRIAD_NAMES[code - 1] for i, code in enumerate(TRICODES)}
 
-def triad201(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
+# ---------------------------------------------------------------------- #
 
-def triad210(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
+trianglesList = []
+currentGraph = nx.DiGraph()
 
-def triad300(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
+for triangle in getting_Triangles(G):
+    trianglesList.append(triangle)
 
-def triad021D(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
 
-def triad111U(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
+for triangle in trianglesList:
+    triangleCode = TRICODE_TO_NAME[tricode(G, triangle[0], triangle[1], triangle[2])]
+    print(triangle, triangleCode)
 
-def triad120D(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-def triad021U(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-def triad030T(currentGraph, numberOfOccurences):
-
-    # 1 to 2, 1 to 3, 3 to 2
-    # Finds this type, number of occurences to reduce search time
-
-    triadOptions = {}
-    items = []
-
-    for i in currentGraph.nodes():
-        items = []
-        counter = 0
-        val = currentGraph.neighbors(i)
-
-        for option in val:
-            items.append(int(option))
-            counter += 1
-
-
-        if counter >= 2:
-
-            triadOptions[int(i)] = items
-
-
-    # Needs to go through list of nodes and neighbors, find those that are
-    # Find those that go from 1 to 2, 1 to 3, 3 to 2
-    # For each node in triadOptions
-    # Assign one as value a, its connections as b and c. If b and c also connect
-
-
-
-    triadFound = False
-    print(triadOptions)
-    aValue = None
-    bValue = None
-    cValue = None
-
-    # avalue is value that goes to bvalue and cvalue
-    # cvalue goes to bvalue
-
-    # Currently not working ? Selecting wrong values
-    # Need to make this work grr
-
-    while triadFound == False:
-
-        triadList = []
-
-        for option in triadOptions: # Looking through each value (potentially A)
-
-            for item in triadOptions[option]:
-
-                triadList.append(item)
-
-            for choice in triadList:
-
-                if (choice in triadOptions and option in triadOptions[choice]):
-
-                    aValue = option
-
-                    cValue = choice
-
-                for myChoice in triadList:
-
-                    if (aValue != None) and (cValue != None) and (myChoice in triadOptions[aValue] and myChoice in triadOptions[cValue]):
-                        bValue = myChoice
-
-
-        if (aValue != None and bValue != None and cValue != None):
-            triadFound = True
-
-    print(aValue, bValue, cValue)
-    new = nx.DiGraph()
-
-    new.add_node(aValue)
-    new.add_node(bValue)
-    new.add_node(cValue)
-    new.add_edge(aValue, bValue)
-    new.add_edge(cValue, aValue)
-    new.add_edge(cValue, bValue)
-
-    plt.cla()
-    nx.draw(new, pos = None, arrows=True, with_labels=True)
-    plt.savefig('Triad Found')
-
-
-
-
-
-
-def triad120U(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-def triad021C(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-def triad030C(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-def triad120C(currentGraph, numberOfOccurences):
-    print("running")
-    # Finds this type, number of occurences to reduce search time
-
-
-g = nx.DiGraph()
-gml = nx.read_gml('internet_routers-22july06.gml')
-
-nx.draw(gml)
-
-
-plt.savefig('example')
-plt.cla()
-currentGraph = nx.read_adjlist('ca-sandi_auths.mtx', create_using = nx.DiGraph())
-nx.draw(currentGraph, pos = None, arrows=True, with_labels=True)
-plt.savefig('mtxExample')
-
-e = nx.triadic_census(currentGraph)
-print(e)
-plt.cla()
-H = currentGraph.subgraph(['1', '58', '45'])
-nx.draw(H, pos = None, arrows=True, with_labels=True)
-plt.savefig('compare')
-
-# Null triad = Type 003
-# Dyadic Traids = Type 012 and 102
-# Connected Triads = 111D, 201, 210, 300, 021D, 111U, 120D, 021U, 030T, 120U, 021C, 030C, 120C
-triadsToFind = ['111D', '201', '210', '300', '021D', '111U', '120D', '021U', '030T', '120U', '021C', '030C', '120C']
-
-for triad in e:
-
-    print('\nTriad Type:', triad, 'Number Of Occurences:', e.get(triad))
-
-    if (triad in triadsToFind) and (e.get(triad) != 0):
-        findTriad(currentGraph, triad, e.get(triad))
+    # This can be what we hand to the front end?
